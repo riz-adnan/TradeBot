f4e42a886a317b5db6e990de2fd3f5ab7de51744
riz-adnan <rizviadnan72@gmail.com>
2025-02-16 07:44:24 +0530

    pitch video

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -46,7 +46,7 @@ export default function Hero() {
             thumbWidth={1024}
             thumbHeight={576}
             thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
+            video="/videos/Trade_Bot_pitch.mp4"
             videoWidth={1920}
             videoHeight={1080} />
 
